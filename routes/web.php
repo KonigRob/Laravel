6fc67f44008f3495de3fb977e3ad356d0c495155
robtallguy <rkoenig5@my.bcit.ca>
2018-04-05 21:38:46 -0700

    Didn't add the virtual hosts

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,6 @@ Route::get('/whatever', function (){
 
 Route::get('contact', 'PagesController@contact');
 Route::get('about', 'PagesController@about');
+
+
+Route::get('posting/{id}', 'PostingController@show');
